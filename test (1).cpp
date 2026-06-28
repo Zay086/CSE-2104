85a23af75049769b3ef113942b22daa4e3615ab5
Evergarden <zuhayer.086@gmail.com>
2026-06-28 11:22:35 +0600

    Offline1.cpp
    Refactor binary search functions for correctness and clarity.

--- test (1).cpp
+++ test (1).cpp
@@ -1,100 +1,102 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int BianrySearch(vector<int>&book_ids, int x){
-    
-    int low=0;
-    int high=book_ids.size();
-    
+int BianrySearch(vector < int > & book_ids, int x) {
+
+    int low = 0;
+    int high = book_ids.size()-1;
+
     int mid;
-    
-    while(low<high){
-        mid=(low-high)/2;
-        
-        if(book_ids[mid]==x){
+
+    while (low < high) {
+        mid = (low + high) / 2;
+
+        if (book_ids[mid] == x) {
             return 1;
         }
-        else if(book_ids[mid]<x){
-            low=mid+1;
+        else if (book_ids[mid] < x) {
+            low = mid + 1;
         }
-        else{
-            high=mid-1;
+        else {
+            high = mid - 1;
         }
     }
-    
-    
+
+
 }
-int UpperBoundSearch(vector<int>&book_ids, int x){
-    
-    int low=0;
-    int high=book_ids.size();
-    
+int UpperBoundSearch(vector < int > & book_ids, int x) {
+
+    int low = 0;
+    int high = book_ids.size();
+
     int mid;
-    
-    while(low<high){
-        mid=(low-high)/2;
-        
-        if(low==high){
+
+    while (low < high) {
+        mid = (low + high) / 2;
+
+        if (low == high) {
             return low;
         }
-        else if(book_ids[mid]<=x){
-            low=mid+1;
+        else if (book_ids[mid] <= x) {
+            low = mid + 1;
         }
-        else{
-            high=mid;
+        else {
+            high = mid;
         }
     }
-    
-    
-int LowerBoundSearch(vector<int>&book_ids, int x){
-    
-    int low=0;
-    int high=book_ids.size();
-    
+    return low;
+}
+
+int LowerBoundSearch(vector < int > & book_ids, int x) {
+
+    int low = 0;
+    int high = book_ids.size();
+
     int mid;
-    
-    while(low<high){
-        mid=(low-high)/2;
-        
-        if(low==high){
+
+    while (low < high) {
+        mid = (low + high) / 2;
+
+        if (low == high) {
             return low;
         }
-        else if(book_ids[mid]>=x){
-            high=mid;
+        else if (book_ids[mid] >= x) {
+            high = mid;
         }
-        else{
-            low=mid+1;
+        else {
+            low = mid + 1;
         }
-    }   
+    }
+    return low;
 
-
+}
 int main() {
-	
-	int n,x,flag=0;
-	vector<int>book_ids;
-	
-	cin>>n;
-	for(int i=0;i<n;i++){
-	    
-	    cin>>n;
-	    book_ids.push_back(x);
-	}
-	
-	cin>>x;
-	
-	if(BianrySearch(&book_ids,x)){
-	    cout<<"Book found"<<endl;
-	    
-	    cout<<"Lower Bound index:"<<LowerBoundSearchSearch(&book_ids,x))<<endl;
-	    
-	    cout<<"Upper Bound index:"<<UpperBoundSearchSearch(&book_ids,x))<<endl;
-	    
-	}else{
-	   cout<<"Book not found"<<endl;
-	}
-	
-	
-	
-	
+
+    int n, x, flag = 0;
+    vector < int > book_ids;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+
+        cin >> x;
+        book_ids.push_back(x);
+    }
+
+    cin >> x;
+
+    if (BianrySearch(book_ids, x)) {
+        cout << "Book found" << endl;
+
+        cout << "Lower Bound index:" << LowerBoundSearch(book_ids, x) << endl;
+
+        cout << "Upper Bound index:" << UpperBoundSearch(book_ids, x) << endl;
+
+    } else {
+        cout << "Book not found" << endl;
+    }
+
+
+
 
 }
